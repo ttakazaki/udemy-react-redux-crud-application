c193ac281286391ee7527fd44ce00f85e47b0051
ttakazaki <urd59675@gmail.com>
2018-05-17 21:41:00 +0900

    apply material-ui to events_new and events_show components

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -3,6 +3,10 @@ import { connect } from 'react-redux'
 import { Field, reduxForm } from 'redux-form'
 import { Link } from 'react-router-dom'
 
+
+import RaisedButton from 'material-ui/RaisedButton'
+import TextField from 'material-ui/TextField'
+
 import { getEvent, deleteEvent, putEvent } from '../actions'
 
 class EventsShow extends Component {
@@ -22,10 +26,14 @@ class EventsShow extends Component {
     const { input, label, type, meta: { touched, error } } = field
 
     return (
-      <div>
-        <input {...input} placeholder={label} type={type} />
-        {touched && error && <span>{error}</span>}
-      </div>
+      <TextField 
+        hintText={label}
+        floatingLabelText={label}
+        type={type}
+        errorText={touched && error}
+        {...input}
+        fullWidth={true}
+      />
     )
   }
 
@@ -43,18 +51,17 @@ class EventsShow extends Component {
 
   render() {
     const { handleSubmit, pristine, submitting, invalid } = this.props
-    console.log(submitting)
+    const style = { margin: 12 }
 
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
         <div><Field label="Title" name="title" type="text" component={this.renderField} /></div>
         <div><Field label="Body" name="body" type="text" component={this.renderField} /></div>
 
-        <div>
-          <input type="submit" value="Submit" disabled={pristine || submitting || invalid} />
-          <Link to="/" >Cancel</Link>
-          <Link to="/" onClick={this.onDeleteClick}>Delete</Link>
-        </div>
+        <RaisedButton label="Submit" type="Submit" style={style} disabled={pristine || submitting || invalid} />
+        <RaisedButton label="Cancel"  style={style} containerElement={<Link to="/" />}/>
+        <RaisedButton label="DELETE"  style={style} onClick={this.onDeleteClick} containerElement={<Link to="/" />}/>
+
       </form>
     )
   }
